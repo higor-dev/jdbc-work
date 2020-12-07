8e6c37faeea1039e95284ee492a7d48a52b60451
higor-dev <madhigor@hotmail.com>
2020-12-07 19:50:44 -0300

    Adding findAll

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,9 +12,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Main method.");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Department department = new Department(null, 2);
+		List<Seller> list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
 		}
 		
 	}
 
-
+}
